2bef4ce58c246bb22f0596783e6d90c37a9b30b7
TimiTao <kunicki.tomasz@gmail.com>
2019-04-16 17:50:29 +0200

    a

---
+++ tests/BasicTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types = 1);
+
+use PHPUnit\Framework\TestCase;
+
+class BasicTest extends TestCase
+{
+    public function testBasic()
+    {
+        self::assertTrue(false, "This should be TRUE");
+    }
+}
