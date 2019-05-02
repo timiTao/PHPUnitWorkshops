0ce27174bf3efe7ac210b029b2a65d9a3f20215a
Marcin <marcingladkowski@gmail.com>
2019-05-02 12:11:17 +0200

    (ref) add two tests for invalid values

--- tests/EventExchangeTest.php
+++ tests/EventExchangeTest.php
@@ -9,4 +9,16 @@ class EventExchangeTest extends TestCase
         $actual = new EventExchange(1.29);
         self::assertEquals(1.29, $actual->getValue());
     }
+
+    public function testThrowExceptionWhenExchangeValueIsLowerThanOneAndBiggerThanZero()
+    {
+        self::expectException(InvalidArgumentException::class);
+        new EventExchange(0.4);
+    }
+
+    public function testThrowExceptionWhenExchangeValueIsLowerThanZero()
+    {
+        self::expectException(InvalidArgumentException::class);
+        new EventExchange(-2.4);
+    }
 }
