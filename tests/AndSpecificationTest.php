511afc67e3fc3746594efea46f37db73a2ab55a5
Marcin <marcingladkowski@gmail.com>
2019-05-02 12:12:56 +0200

    (ref) change to mocks

--- tests/AndSpecificationTest.php
+++ tests/AndSpecificationTest.php
@@ -1,33 +1,45 @@
 <?php
 
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
 class AndSpecificationTest extends TestCase
 {
+    /** @var Bid | MockObject */
+    private $bid;
+    /** @var EventDateSpecification */
+    private $bidDateSpec;
+
+    protected function setUp()
+    {
+        $this->bid = self::createMock(Bid::class);
+        $this->bidDateSpec = new EventDateSpecification();
+    }
 
     public function testShouldReturnTrueWhenTwoSpecAreReturnTrue()
     {
-        $bid = new Bid(new Client(new Country('Poland')), new EventName('Football'), new EventDate('2019-05-01'), new EventExchange(1.30));
+        $now = new \DateTime();
+        $tommorow = $now->modify('+1day')->format('Y-m-d');
         $client = new Client(new Country('Poland'));
 
-        $bidDateSpec = new EventDateSpecification();
+        $this->bid->method('getEventDate')->willReturn(new EventDate($tommorow));
+        $this->bid->method('getClient')->willReturn($client);
+
         $clientSpec = new ClientSpecification($client);
+        $andSpec = new AndSpecification($this->bidDateSpec, $clientSpec);
 
-        $andSpec = new AndSpecification($bidDateSpec, $clientSpec);
-
-        self::assertTrue($andSpec->isSatisfiedBy($bid));
+        self::assertTrue($andSpec->isSatisfiedBy($this->bid));
     }
 
     public function testShouldReturnFalseWhenOneOfSpecIsIncorrect()
     {
-        $bid = new Bid(new Client(new Country('Poland')), new EventName('Football'), new EventDate('2019-05-01'), new EventExchange(1.30));
+        $this->bid->method('getEventDate')->willReturn(new EventDate('1970-01-01'));
+        $this->bid->method('getClient')->willReturn(new Client(new Country('Poland')));
+
         $client = new Client(new Country('USA'));
-
-        $bidDateSpec = new EventDateSpecification();
         $clientSpec = new ClientSpecification($client);
+        $andSpec = new AndSpecification($this->bidDateSpec, $clientSpec);
 
-        $andSpec = new AndSpecification($bidDateSpec, $clientSpec);
-
-        self::assertFalse($andSpec->isSatisfiedBy($bid));
+        self::assertFalse($andSpec->isSatisfiedBy($this->bid));
     }
 }
