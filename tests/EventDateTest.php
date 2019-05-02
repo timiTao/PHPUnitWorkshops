2153ba7d5e8d5e99634c448551b73a1b1ead4152
Marcin <marcingladkowski@gmail.com>
2019-05-02 12:12:01 +0200

    (ref) add test for invalid format date

--- tests/EventDateTest.php
+++ tests/EventDateTest.php
@@ -4,11 +4,25 @@ use PHPUnit\Framework\TestCase;
 
 class EventDateTest extends TestCase
 {
-        public function testShouldCreateValidEventDate()
-        {
-            $now = new \DateTime();
-            $formatedNow = $now->format('Y-m-d');
-            $eventDate = new EventDate($formatedNow);
-            self::assertEquals($formatedNow, $eventDate->getDate()->format('Y-m-d'));
-        }
+    /** @var DateTime */
+    private $now;
+
+    protected function setUp()
+    {
+        $this->now = new \DateTime();
+    }
+
+    public function testShouldCreateValidEventDate()
+    {
+        $dateNow = $this->now->format('Y-m-d');
+        $eventDate = new EventDate($dateNow);
+        self::assertEquals($dateNow, $eventDate->getDate()->format('Y-m-d'));
+    }
+
+    public function testShouldThrowExceptionWhenDateTimeFormatIsInvalid()
+    {
+        self::expectException(InvalidArgumentException::class);
+        $dateNow = $this->now->format('Y-m-d H:i:s');
+        new EventDate($dateNow);
+    }
 }
