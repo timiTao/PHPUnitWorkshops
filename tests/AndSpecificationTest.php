5628c862a8938446fd66d7ce0ba575499c3f4271
Marcin <marcingladkowski@gmail.com>
2019-07-02 23:41:55 +0200

    (test) change tests implementation to mocks

--- tests/AndSpecificationTest.php
+++ tests/AndSpecificationTest.php
@@ -7,38 +7,31 @@ class AndSpecificationTest extends TestCase
 {
     /** @var Bid | MockObject */
     private $bid;
-    /** @var EventDateSpecification */
-    private $bidDateSpec;
+    /** @var SpecificationInterface | MockObject $specificationInterfaceMock */
+    private $specificationInterfaceMock;
 
     protected function setUp()
     {
         $this->bid = self::createMock(Bid::class);
-        $this->bidDateSpec = new EventDateSpecification();
+        $this->specificationInterfaceMock = self::createMock(SpecificationInterface::class);
     }
 
     public function testShouldReturnTrueWhenTwoSpecAreReturnTrue()
     {
-        $now = new \DateTime();
-        $tommorow = $now->modify('+1day')->format('Y-m-d');
-        $client = new Client(new Country('Poland'));
+        $this->specificationInterfaceMock->method('isSatisfiedBy')->willReturn(true);
 
-        $this->bid->method('getEventDate')->willReturn(new EventDate($tommorow));
-        $this->bid->method('getClient')->willReturn($client);
-
-        $clientSpec = new ClientSpecification($client);
-        $andSpec = new AndSpecification($this->bidDateSpec, $clientSpec);
+        $andSpec = new AndSpecification($this->specificationInterfaceMock, $this->specificationInterfaceMock);
 
         self::assertTrue($andSpec->isSatisfiedBy($this->bid));
     }
 
     public function testShouldReturnFalseWhenOneOfSpecIsIncorrect()
     {
-        $this->bid->method('getEventDate')->willReturn(new EventDate('1970-01-01'));
-        $this->bid->method('getClient')->willReturn(new Client(new Country('Poland')));
+        /** @var SpecificationInterface | MockObject $specificationInterfaceInvalidMock */
+        $specificationInterfaceInvalidMock = self::createMock(SpecificationInterface::class);
+        $specificationInterfaceInvalidMock->method('isSatisfiedBy')->willReturn(false);
 
-        $client = new Client(new Country('USA'));
-        $clientSpec = new ClientSpecification($client);
-        $andSpec = new AndSpecification($this->bidDateSpec, $clientSpec);
+        $andSpec = new AndSpecification($specificationInterfaceInvalidMock, $this->specificationInterfaceMock);
 
         self::assertFalse($andSpec->isSatisfiedBy($this->bid));
     }
